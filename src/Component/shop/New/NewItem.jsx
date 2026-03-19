5110f3b6c402fe144b167d3939e4cab049bb9c61
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-20 01:37:23 +0300

    Update NewItem.jsx

--- src/Component/shop/New/NewItem.jsx
+++ src/Component/shop/New/NewItem.jsx
@@ -1,37 +1,57 @@
-import React from 'react'
-import "./NewItem.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartShopping , faHeartCircleCheck} from '@fortawesome/free-solid-svg-icons'
-import { faHeart } from '@fortawesome/free-regular-svg-icons'
-import {useShoppingCart} from "../../../Context/Context"
-import { Link } from 'react-router-dom'
-const NewItem = ({id,image,company,model,price }) => {
-  const {increaseCartQuantity,addToFav,deleteFav,Favorites} = useShoppingCart();
-  const FavChecker = (id) =>{
-    const boolean = Favorites.some((item) => item.id === id);
-    return boolean;
-  };
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus, faHeartCircleCheck, faEye } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { useShoppingCart } from "../../../Context/Context";
+import { Link } from 'react-router-dom';
+import "./NewItem.css";
+
+const NewItem = ({id, image, company, model, price }) => {
+  const { increaseCartQuantity, addToFav, deleteFav, Favorites } = useShoppingCart();
+  
+  const isFavorite = Favorites.some((item) => item.id === id);
+
   return (
-    <div className='NewItem text-center'>
-      <Link className='text-decoration-none' to={`/SinglePhones/${model}`}>
-        <img src={image.one}  />
-        <h4>{company}</h4>
-        <h3>{model}</h3>
-        <p>${price}</p>
-      </Link>
-      <div className='NewItem-slid'>
-          <div className='NewItem-menu'>
-              <button className='slid-cart' onClick={() => increaseCartQuantity(id)}><FontAwesomeIcon icon={faCartShopping} className='fs-2' style={{color: "rgb(55, 115, 255)",}} /></button>
-              {FavChecker(id) ? (
-              <button className='slid-fav' onClick={() => deleteFav(id)}><FontAwesomeIcon  icon={faHeartCircleCheck} className='fs-2' style={{color: "rgb(55, 115, 255)",}} /></button>
-              ):( 
-              <button className='slid-fav' onClick={() => addToFav(id)}><FontAwesomeIcon  icon={faHeart} className='fs-2' style={{color: "rgb(55, 115, 255)",}} /></button>
-              )}
-          </div>
-          
+    <div className='product-card-modern'>
+      <div className='product-image-area'>
+        <Link to={`/SinglePhones/${model}`}>
+            <img src={image.one} alt={model} className="product-img" />
+        </Link>
+        
+        {/* أزرار سريعة تظهر عند التمرير */}
+        <div className='product-quick-actions'>
+            <button className='action-btn cart' title="Add to Cart" onClick={() => increaseCartQuantity(id)}>
+                <FontAwesomeIcon icon={faCartPlus} />
+            </button>
+            
+            {isFavorite ? (
+                <button className='action-btn fav active' title="Remove from Wishlist" onClick={() => deleteFav(id)}>
+                    <FontAwesomeIcon icon={faHeartCircleCheck} />
+                </button>
+            ) : (
+                <button className='action-btn fav' title="Add to Wishlist" onClick={() => addToFav(id)}>
+                    <FontAwesomeIcon icon={faHeart} />
+                </button>
+            )}
+
+            <Link to={`/SinglePhones/${model}`} className='action-btn view' title="View Details">
+                <FontAwesomeIcon icon={faEye} />
+            </Link>
+        </div>
+      </div>
+
+      <div className='product-info-area'>
+        <span className='brand-name'>{company}</span>
+        <Link className='model-link' to={`/SinglePhones/${model}`}>
+            <h3 className='model-name'>{model}</h3>
+        </Link>
+        <div className='price-wrapper'>
+            <span className='currency'>$</span>
+            <span className='amount'>{price}</span>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default NewItem
+export default NewItem;

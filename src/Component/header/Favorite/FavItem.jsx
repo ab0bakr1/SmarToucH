fb7db577d7c14d331ce406337986b3f5720d2525
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-19 22:56:27 +0300

    Refactor FavItem component layout and styling

--- src/Component/header/Favorite/FavItem.jsx
+++ src/Component/header/Favorite/FavItem.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import {useShoppingCart} from "../../../Context/Context"
-import AllPhones from "../../../Data/AllPhones.json"
-import "./FavItem.css"
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useShoppingCart } from "../../../Context/Context";
+import AllPhones from "../../../Data/AllPhones.json";
+import { Link } from 'react-router-dom';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./FavItem.css";
 
-const FavItem = ({ id}) => {
+const FavItem = ({ id }) => {
     const { deleteFav } = useShoppingCart();
     const item = AllPhones.find((i) => i.id === id);
     if (item == null) return null;
-  return (
-    <div className='d-flex gap-1 my-3 sticky'>
-      <Link to={`/SinglePhones/${item.model}`} style={{width:"100px", height:"100px", minWidth:"100px"}}>
-        <img className='w-100 h-100' src={item.image.one}  draggable/>
-      </Link>
-      <div className='w-100'>
-        <Link className='text-decoration-none text-dark' to={`/SinglePhones/${item.model}`}>
-          <h3 className='font fs-5 text-start'>{item.company} {item.model}</h3>
-        </Link>
-        <div className='d-flex justify-content-start'>
-          <h3 className='font fs-5 m-0' style={{color:"rgb(55, 115, 255)"}}><b style={{color:"black"}}>Price:</b>${item.price}</h3>
+
+    return (
+        <div className='modern-fav-item'>
+            <div className='fav-img-wrapper'>
+                <img src={item.image.one} alt={item.model} />
+            </div>
+            
+            <div className='fav-info'>
+                <div className='fav-text-content'>
+                    <Link to={`/SinglePhones/${item.model}`} className='fav-link'>
+                        <span className='brand-label'>{item.company}</span>
+                        <h5 className='model-name'>{item.model}</h5>
+                    </Link>
+                    <div className='fav-price'>${item.price}</div>
+                </div>
+                
+                <button className='fav-remove-action' onClick={() => deleteFav(id)} title="Remove">
+                    <FontAwesomeIcon icon={faTrashCan} />
+                </button>
+            </div>
         </div>
-        <button className='font px-4 py-0 fs-5 w-75 btn' onClick={()=>deleteFav(id)} style={{border:"none",backgroundColor:"rgb(55, 115, 255)",color:"white",borderRadius:"20px"}}>Delete</button>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default FavItem
+export default FavItem;

de09f46da0ee4ef76c51518d780dba645512ca3e
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-20 01:47:33 +0300

    Update AllShop.jsx

--- src/Component/shop/AllShop/AllShop.jsx
+++ src/Component/shop/AllShop/AllShop.jsx
@@ -4,26 +4,35 @@ import { Col, Container, Row } from 'react-bootstrap'
 import Footer from '../../Footer/Footer'
 import AllPhones from "../../../Data/AllPhones.json"
 import AllShopItem from './AllShopItem'
+import "./AllShop.css"
 
 const AllShop = () => {
   return (
-    <>
-    <Header />
-    <Container>
-        <section>
-            <h2 className='text-center text-uppercase my-4'>shop all</h2>
-            <Row className='' lg={4} md={2} sm={1} xs={1}>
-                {AllPhones.map((item) =>(
-                    <Col>
+    <div className="all-shop-page">
+      <Header />
+      
+      {/* هيدر الصفحة العلوي */}
+      <div className="shop-page-banner">
+        <Container>
+           <h1 className='shop-page-title'>Explore Our Collection</h1>
+           <p className='shop-page-subtitle'>Find the perfect smartphone for your lifestyle</p>
+        </Container>
+      </div>
+
+      <Container className="my-5">
+        <section className="all-products-grid">
+            <Row className='g-4' lg={4} md={3} sm={2} xs={1}>
+                {AllPhones.map((item) => (
+                    <Col key={item.id} className="d-flex align-items-stretch">
                         <AllShopItem {...item}/>
                     </Col>
                 ))}
             </Row>
         </section>
-    </Container>
-    <Footer />
-    </>
-    
+      </Container>
+      
+      <Footer />
+    </div>
   )
 }
 

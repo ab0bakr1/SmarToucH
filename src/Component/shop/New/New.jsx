00b9f174200b7b914f7efc287dba3b7063e26b02
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-20 01:36:20 +0300

    Update New.jsx

--- src/Component/shop/New/New.jsx
+++ src/Component/shop/New/New.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import "./New.css"
-import AllPhones from "../../../Data/AllPhones.json"
-import { Col, Row } from 'react-bootstrap'
-import NewItem from './NewItem'
+import React from 'react';
+import { Col, Row, Container } from 'react-bootstrap';
+import AllPhones from "../../../Data/AllPhones.json";
+import NewItem from './NewItem';
+import "./New.css";
 
-const New  = () => {
+const New = () => {
   return (
-    <>
-    <div className='new-title'>
-        <h2>new <span className="title1" >Smar</span><span className="title2">T</span><span className="title3">oucH</span></h2>
-    </div>
-    <Row className='mx-auto' lg={4} md={2} sm={1} xs={1}>
-        {AllPhones.slice(-8).map((item) =>(
-            <Col className='my-3' key={item.id}>
-                <NewItem {...item}/>
-            </Col>
-        ))}
+    <section className="new-arrivals-section py-5">
+      <Container>
+        <div className='new-section-header mb-5'>
+            <span className="subtitle">Latest Collections</span>
+            <h2 className="main-title">
+                New <span className="t1">Smar</span><span className="t2">T</span><span className="t3">oucH</span>
+            </h2>
+            <div className="title-underline"></div>
+        </div>
 
-    </Row>
-    </>
-  )
+        <Row className='g-4' lg={4} md={2} sm={1} xs={1}>
+            {AllPhones.slice(-8).reverse().map((item) => (
+                <Col key={item.id}>
+                    <NewItem {...item}/>
+                </Col>
+            ))}
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
-export default New 
+export default New;

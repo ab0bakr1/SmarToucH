fe041f0513e3ed1ae62ad48b86675b96948281f9
Abobakr Almashhor <121812837+ab0bakr1@users.noreply.github.com>
2026-03-20 01:44:00 +0300

    Update Shop.jsx

--- src/Component/shop/Shop.jsx
+++ src/Component/shop/Shop.jsx
@@ -2,18 +2,26 @@ import React from 'react'
 import New from './New/New'
 import "./Shop.css"
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 const Shop = () => {
   return (
-    <>
-    <New />
-    <div className="btn-ShopAll">
-      <Link to="/Shop-All">
-        <button>Shop All</button>
-    </Link>
-    </div>
-    
-    </>
+    <section className="shop-section-wrapper">
+      {/* معرض المنتجات الجديد */}
+      <New />
+      
+      {/* منطقة زر عرض الكل */}
+      <div className="shop-all-container">
+        <Link to="/Shop-All" className="shop-all-link">
+          <button className="modern-shop-all-btn">
+            <span>Explore All Devices</span>
+            <FontAwesomeIcon icon={faArrowRightLong} className="btn-arrow" />
+          </button>
+        </Link>
+        <p className="shop-hint">Over 100+ new models waiting for you</p>
+      </div>
+    </section>
   )
 }
 
